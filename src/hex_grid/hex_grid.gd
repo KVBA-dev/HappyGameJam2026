6df29fc58e32f2bac58dd295b489743ec2982979
Truyka <mmilewski2202@gmail.com>
2026-08-25 00:33:50 +0200

    Fill surroundings with hexagons helper function

--- src/hex_grid/hex_grid.gd
+++ src/hex_grid/hex_grid.gd
@@ -9,12 +9,27 @@ func _ready():
 	GameManager.hex_grid = self
 	hex_map[HexVector.ZERO] = root_hex
 
+func surround_with_hexes(radius: int):
+	var blank = preload("res://const_data/hexes/blank_hex.tres")
+
+	for direction: HexVector in HexVector.DIRECTION_MAP.values():
+		for i in range(radius):
+			var hex_vec = direction.mult(i+1)
+			spawn_hex_at(hex_vec, blank)
+
+			for j in range(radius-i):
+				var final = hex_vec.add(direction.rotated(1).mult(j))
+				spawn_hex_at(final, blank)
+
+
+
 func get_hex_at(_position: HexVector) -> Hex:
 	return hex_map.get(_position, null)
 
 func spawn_hex_at(_position: HexVector, hex_type: HexData) -> Hex:
 	if hex_map.has(_position):
 		ErrorBus.hex_already_exist_on_position.emit(_position)
+		print("lol")
 		return
 	
 	var new_hex := Hex.new_instance(_position, hex_type)
