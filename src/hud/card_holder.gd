5ed762fd451b76037fddcd3f4149a006598373b3
Truyka <mmilewski2202@gmail.com>
2026-08-24 19:20:51 +0200

    Card preview done

--- src/hud/card_holder.gd
+++ src/hud/card_holder.gd
@@ -7,8 +7,7 @@ var currently_focused: CardHudHighlight = null
 
 func _process(_delta: float) -> void:
 	var closest := find_closest_hovered()
-	if not currently_focused or currently_focused.hover == false:
-		_set_currently_focused(closest)
+	_set_currently_focused(closest)
 
 func _set_currently_focused(node: CardHudHighlight):
 	if currently_focused:
@@ -44,14 +43,8 @@ func reorder_cards() -> void:
 
 	var cards: Array = cards_container.get_children()
 	for idx in range(len(cards)):
-		cards[idx].position = calculate_card_transform(cards_position_data[idx].rotation)
-		cards[idx].rotation = cards_position_data[idx].rotation
-
-func calculate_card_transform(angle_offset: float) -> Vector2:
-	const up_angle: float = -PI / 2.0
-	var center_distance: float = abs(placer_center.position.y)
-
-	return center_distance * Vector2.from_angle(up_angle + angle_offset)
+		cards[idx].position = cards_position_data[idx].position
+		cards[idx].rotation = cards_position_data[idx].rotation / 2.0
 
 func get_position_data_for_cards() -> Array[Dictionary]:
 	const up_angle: float = -PI / 2.0
@@ -59,19 +52,28 @@ func get_position_data_for_cards() -> Array[Dictionary]:
 	var cards := get_cards()
 	var center_distance: float = abs(placer_center.position.y)
 
-	var angle_card_distance = 0.15
+	const angle_card_distance = 0.15
+	const angle_hover_distance = 0.3
 
 	@warning_ignore("integer_division")
 	var angle_offset: float = -int(len(cards) / 2) * angle_card_distance
 	if len(cards) % 2 == 0:
 		angle_offset += angle_card_distance / 2.0
+	if currently_focused:
+		angle_offset -= angle_hover_distance / 2.0
 
 	var calculated_card_data: Array[Dictionary] = []
-	for card: Node2D in cards:
+	for card: Node in cards:
+		if card == currently_focused:
+			angle_offset += angle_hover_distance / 2.0
+
 		var data_dict = {
 			position = center_distance * Vector2.from_angle(up_angle + angle_offset),
 			rotation = angle_offset / 2.0
 		}
+
+		if card == currently_focused:
+			angle_offset += angle_hover_distance / 2.0
 
 		calculated_card_data.push_back(data_dict)
 		angle_offset += angle_card_distance
--- src/hud/card_hud_highlight.gd
+++ src/hud/card_hud_highlight.gd
@@ -7,28 +7,44 @@ enum State {
 }
 
 @onready var card_holder: CardHolder = get_tree().get_first_node_in_group("card_holder")
-var state: State = State.IN_HAND
+var _state: State = State.IN_HAND
+var state: State:
+	get:
+		return _state
+	set(value):
+		_state = value
+		_on_state_change()
+
 var _hover: bool = false
 var hover: bool:
 	get:
 		return _hover
 
 func _process(_delta: float) -> void:
-	match state:
+	match _state:
 		State.IN_HAND: _in_hand_processed()
 		State.DRAGGED: pass
 		State.PREVIEWED: _previewed_process()
 
 func _in_hand_processed():
-	scale = Vector2.ONE
 	pass
 
 func _previewed_process():
-	scale = Vector2.ONE * 1.2
 	pass
+
 func _on_area_2d_mouse_entered() -> void:
 	_hover = true
 
 func _on_area_2d_mouse_exited() -> void:
 	_hover = false
 
+func _on_state_change():
+	match _state:
+		State.IN_HAND: 
+			scale = Vector2.ONE 
+			z_index = 0
+		State.PREVIEWED:
+			scale = Vector2.ONE * 1.2
+			rotation = 0
+			z_index = 10
+		State.DRAGGED: pass
