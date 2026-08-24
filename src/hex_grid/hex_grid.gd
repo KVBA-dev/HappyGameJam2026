02b4af4be59e5d66a3b3ffeaff7197a4afe574b6
KVBA-dev <kubaokon8@gmail.com>
2026-08-24 18:05:21 +0200

    added item and recipe classes

--- src/hex_grid/hex_grid.gd
+++ src/hex_grid/hex_grid.gd
@@ -5,19 +5,19 @@ var hex_map: Dictionary[HexVector, Hex] = {}
 signal hex_changed(hex: Hex)
 
 func _ready():
-    GameManager.hex_grid = self
-    hex_map[HexVector.ZERO] = root_hex
+	GameManager.hex_grid = self
+	hex_map[HexVector.ZERO] = root_hex
 
 func get_hex_at(_position: HexVector) -> Hex:
-    return hex_map.get(_position, null)
+	return hex_map.get(_position, null)
 
 func spawn_hex_at(_position: HexVector, hex_type: HexData) -> Hex:
-    if hex_map.has(_position):
-        ErrorBus.hex_already_exist_on_position.emit(_position)
-        return
-    
-    var new_hex := Hex.new_instance(_position, hex_type)
-    root_hex.add_child(new_hex)
-    hex_map[_position] = new_hex
-    hex_changed.emit(new_hex)
-    return new_hex
+	if hex_map.has(_position):
+		ErrorBus.hex_already_exist_on_position.emit(_position)
+		return
+	
+	var new_hex := Hex.new_instance(_position, hex_type)
+	root_hex.add_child(new_hex)
+	hex_map[_position] = new_hex
+	hex_changed.emit(new_hex)
+	return new_hex
