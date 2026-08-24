d15e9103737ddc6072027b1928f8ba2539532cc9
Truyka <mmilewski2202@gmail.com>
2026-08-25 00:32:59 +0200

    Added many utility functions

--- src/hex_vector/hex_vector.gd
+++ src/hex_vector/hex_vector.gd
@@ -19,12 +19,21 @@ enum Direction {
 }
 static var DIRECTION_MAP: Dictionary[Direction, HexVector] = {
 	Direction.UP_RIGHT: HexVector.new(1, -1),
-	Direction.UP_LEFT: HexVector.new(-1, 0),
+	Direction.DOWN_LEFT: HexVector.new(-1, 1),
+
 	Direction.CENTER_RIGHT: HexVector.new(1, 0),
-	Direction.CENTER_LEFT: HexVector.new(-1, 1),
+	Direction.CENTER_LEFT: HexVector.new(-1, 0),
+
+	Direction.UP_LEFT: HexVector.new(0, -1),
 	Direction.DOWN_RIGHT: HexVector.new(0, 1),
-	Direction.DOWN_LEFT: HexVector.new(0, -1)
 }
+
+static func UP_RIGHT() -> HexVector: return direction_vector(Direction.UP_RIGHT)
+static func UP_LEFT() -> HexVector: return direction_vector(Direction.UP_LEFT)
+static func CENTER_RIGHT() -> HexVector: return direction_vector(Direction.CENTER_RIGHT)
+static func CENTER_LEFT() -> HexVector: return direction_vector(Direction.CENTER_LEFT)
+static func DOWN_RIGHT() -> HexVector: return direction_vector(Direction.DOWN_RIGHT)
+static func DOWN_LEFT() -> HexVector: return direction_vector(Direction.DOWN_LEFT)
 
 static var ZERO: HexVector:
 	get():
@@ -53,11 +62,28 @@ func distance_to(other: HexVector) -> int:
 	var ds: int = abs(s() - other.s())
 	return (dq + dr + ds) / 2
 
+func rotated(n: int) -> HexVector:
+	n = posmod(n, 6)
+	match n:
+		0: return HexVector.new(q, r)
+		1: return HexVector.new(-r, -s())
+		2: return HexVector.new(-q-r, q)
+		3: return HexVector.new(-q, -r)
+		4: return HexVector.new(r, -q-r)
+		5: return HexVector.new(q+r, -q)
+	return null	
+
 func add(other: HexVector) -> HexVector:
 	return HexVector.new(q + other.q, r + other.r)
 
 func sub(other: HexVector) -> HexVector:
 	return HexVector.new(q - other.q, r - other.r)
+
+func comp(other: HexVector) -> bool:
+	return q == other.q and r == other.r
+
+func mult(val: int) -> HexVector:
+	return HexVector.new(q * val, r * val)
 
 func to_pixel(hex_width: float = HEX_WIDTH, hex_height: float = HEX_HEIGHT) -> Vector2:
 	var x: float = hex_width * (q + r / 2.0)
