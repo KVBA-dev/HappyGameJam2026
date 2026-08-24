c3d67fc6a9c93ae192213d4130fd8e33dfd56519
GregN <grzegorz.niespodzianek@gmail.com>
2026-08-24 18:32:27 +0200

    hex finished

---
+++ src/utils/tooltips/text_tooltip/text_tooltip.gd
@@ -0,0 +1,14 @@
+class_name TextTooltip extends Tooltip
+
+@onready var text_label: Label = %TextLabel
+const SCENE := preload("uid://bjwylgmbh648b")
+var text: String
+
+static func new_instance(_text: String) -> TextTooltip:
+    var tooltip: TextTooltip = SCENE.instantiate()
+    tooltip.text = _text
+    return tooltip
+
+
+func _ready() -> void:
+    text_label.text = text
