fb88e09e30efd92291cab2eb018357e7fb113c9d
Truyka <mmilewski2202@gmail.com>
2026-08-25 20:48:44 +0200

    Bugfixed hex vectors

--- src/hex_vector/hex_vector.gd
+++ src/hex_vector/hex_vector.gd
@@ -1,8 +1,17 @@
 class_name HexVector extends RefCounted
 
 
-var q: int
-var r: int
+var vec: Vector2i
+var q: int:
+	get:
+		return vec.x
+	set(val):
+		vec.x = val
+var r: int:
+	get:
+		return vec.y
+	set(val):
+		vec.y = val
 
 func s() -> int:
 	return -q - r
@@ -37,33 +46,20 @@ static func DOWN_LEFT() -> HexVector: return direction_vector(Direction.DOWN_LEF
 
 static var ZERO: HexVector:
 	get():
-		return HexVector.new_instance(0, 0)
+		return HexVector.new(0, 0)
 
 static func direction_vector(direction: Direction) -> HexVector:
 	return DIRECTION_MAP[direction] 
 
 func neighbor(direction: Direction) -> HexVector:
 	var d := direction_vector(direction)
-	return HexVector.new_instance(q + int(d.x), r + int(d.y))
+	return HexVector.new(q + int(d.q), r + int(d.r))
 
 func get_all_neighbors() -> Array[HexVector]:
 	var result: Array[HexVector] = []
 	for direction in Direction.values():
 		result.append(neighbor(direction))
 	return result
-
-static func new_instance(q: int, r: int) -> HexVector:
-	var vector: HexVector
-	for v: HexVector in vectors:
-		if v.q == q and v.r == r:
-			vector = v
-			break
-	if vector:
-		return vector
-	vector = HexVector.new(q, r)
-	vectors.append(vector)
-	return vector
-
 
 func _init(_q: int, _r: int):
 	q = _q
@@ -87,13 +83,13 @@ func rotated(n: int) -> HexVector:
 	return null	
 
 func add(other: HexVector) -> HexVector:
-	return HexVector.new_instance(q + other.q, r + other.r)
+	return HexVector.new(q + other.q, r + other.r)
 
 func sub(other: HexVector) -> HexVector:
-	return HexVector.new_instance(q - other.q, r - other.r)
+	return HexVector.new(q - other.q, r - other.r)
 
 func comp(other: HexVector) -> bool:
-	return self == other # There can be only single instance with specified coords
+	return vec == other.vec
 
 func mult(val: int) -> HexVector:
 	return HexVector.new(q * val, r * val)
