09c81a6a1a534d145aa5574bf6660863b7cb5632
KVBA-dev <kubaokon8@gmail.com>
2026-08-25 22:15:49 +0200

    added audio system functions

--- src/audio/audio_system.gd
+++ src/audio/audio_system.gd
@@ -2,7 +2,10 @@ class_name AudioSystem
 extends Node
 
 @onready var music_player: AudioStreamPlayer = $Music
+@onready var ambience_player: AudioStreamPlayer = $Ambience
 @onready var sfx_player: AudioStreamPlayer = $SFX
+
+static var instance: AudioSystem
 
 enum SFXType {
 	HOVER,
@@ -13,3 +16,22 @@ enum SFXType {
 
 @export var sfx_sounds: Dictionary[SFXType, AudioStream]
 @export var music_streams: Array[AudioStream]
+
+var music_index: int = 0
+
+func _ready() -> void:
+	instance = self
+	music_index = 0
+	music_player.finished.connect(func(): 
+		music_index = (music_index + 1) % len(music_streams)
+		load_and_play_music()
+	)
+	load_and_play_music()
+
+func load_and_play_music() -> void:
+	music_player.stream = music_streams[music_index]
+	music_player.play()
+
+func play_sfx(type: SFXType) -> void:
+	sfx_player.stream = sfx_sounds[type]
+	sfx_player.play()
