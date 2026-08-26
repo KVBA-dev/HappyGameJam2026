82009c673952ca46f74fba228f493efdc6e183e9
Truyka <mmilewski2202@gmail.com>
2026-08-26 19:03:49 +0200

    Better rotations

--- src/hex/hex_sprite.gd
+++ src/hex/hex_sprite.gd
@@ -10,14 +10,22 @@ func init(hex_data: HexData):
 	on_hex_sprite.texture = hex_data.on_surface_texture
 	below_sprite.texture = hex_data.below_texture
 
+	print(below_sprite.position)
+
 
 
 @onready var _rotate_timer: Timer = %RotateTimer
 @onready var _below_displacement: Node2D = %BelowDisplacement
+@onready var _bg_displacement: Node2D = %BgDisplacement
 var _is_rotating: bool = false
 var _displace_tween: Tween
+var _rotation_tween: Tween
+var _n_60degree_rotations: int = 0
+var n_60degree_rotations: int:
+	get: return _n_60degree_rotations
 
 func rotate_left() -> void:
+	_n_60degree_rotations -= 1
 	if animation_player.is_playing():
 		_rotate_timer.stop()
 		animation_player.stop()
@@ -26,6 +34,7 @@ func rotate_left() -> void:
 		animation_player.play("rotate_left")
 
 func rotate_right() -> void:
+	_n_60degree_rotations += 1
 	if animation_player.is_playing():
 		_rotate_timer.stop()
 		animation_player.stop()
@@ -37,6 +46,18 @@ func _on_animation_started(_anim_name: String) -> void:
 	if _anim_name.contains("rotate"):
 		_is_rotating = true
 		_rotate_timer.start()
+
+		if _rotation_tween:
+			_rotation_tween.kill()
+
+		var saved_rot = _bg_displacement.rotation
+		_rotation_tween = get_tree().create_tween()
+		_rotation_tween.tween_method(func(weight: float):
+			_bg_displacement.rotation = lerp_angle(
+				saved_rot, 
+				_n_60degree_rotations * PI/3, weight),
+			0.0, 1.0, 0.24
+		)
 
 func _on_rotate_timer_timeout() -> void:
 	_is_rotating = false
