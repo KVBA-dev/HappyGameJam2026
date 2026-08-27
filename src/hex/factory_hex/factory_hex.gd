cbee2b432d114e37075a797fd1cabf0061caca06
GregN <grzegorz.niespodzianek@gmail.com>
2026-08-27 23:23:47 +0200

    deleted storage feature from factory hex

--- src/hex/factory_hex/factory_hex.gd
+++ src/hex/factory_hex/factory_hex.gd
@@ -2,8 +2,6 @@ class_name FactoryHex extends Hex
 
 @onready var indicator_container: Node2D = %IndicatorContainer
 
-var in_production := false
-var storage: Dictionary[ItemData, int]
 var recipe: Recipe
 var paths: Array[PathData] = []
 var ticks: int
@@ -31,12 +29,9 @@ func _ready() -> void:
 
 	recipe = hex_data.recipe
 	ticks = recipe.processing_time_ticks
-	for item: ItemData in recipe.requirements.keys():
-		storage[item] = 0
 	SignalBus.game_timer_tick.connect(tick)
 	GameManager.paths.path_created.connect(_on_path_created)
 	GameManager.paths.path_deleted.connect(_on_path_deleted)
-
 func _process(_delta: float) -> void:
 	if CursorSelect.selected == self:
 		indicator_container.show()
@@ -48,8 +43,6 @@ func _process(_delta: float) -> void:
 func _on_path_created(path_data: PathData):
 	if path_data.start == self:
 		paths.append(path_data)
-		_start_production()
-
 	_update_connected()
 
 func _on_path_deleted(path_data: PathData):
@@ -70,30 +63,13 @@ func _update_connected() -> void:
 			return
 	connected.value = false
 
-	
-
 func on_item_input(item: Item):
 	if item.item_data in recipe.requirements:
-		consume(item.item_data)
 		item.get_consumed()
 
-func consume(item: ItemData):
-	storage[item] += 1
-	_start_production()
 	
-func _is_enough_in_storage() -> bool:
-	for requirement: ItemData in recipe.requirements.keys():
-		if storage[requirement] < recipe.requirements[requirement]:
-			return false
-	return true
-
-func _start_production():
-	if in_production or not _is_enough_in_storage():
-		return
-	in_production = true
-
 func tick():
-	if not in_production:
+	if not connected.value:
 		return
 	ticks -= 1
 	if ticks == 0:
@@ -101,16 +77,12 @@ func tick():
 		produce()
 
 func produce():
-	in_production = false
+	if not connected.value:
+		return
 
 	_spawn_product()
-	if _is_enough_in_storage():
-		_start_production()
 
 func _spawn_product():
-	for requirement: ItemData in recipe.requirements.keys():
-		storage[requirement] -= recipe.requirements[requirement]
-	
 	GameManager.paths.spawn_item_on_path(recipe.produces, paths.pick_random())
 	SignalBus.item_produced.emit(recipe.produces)
 
