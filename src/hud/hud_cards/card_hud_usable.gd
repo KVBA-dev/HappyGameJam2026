58d63534552d19783dadb306a6bab008a998bea0
Truyka <mmilewski2202@gmail.com>
2026-08-28 19:16:33 +0200

    usable card animations

--- src/hud/hud_cards/card_hud_usable.gd
+++ src/hud/hud_cards/card_hud_usable.gd
@@ -5,7 +5,19 @@ func start_use_animation():
 	var target := Cursor.hover_hex
 	# TODO-r: Make it so hexes can't be placed on same tile during animation
 	var grid_pos := target.hex_position
-	SignalBus.card_used.emit(self, grid_pos)
-	queue_free()
 
+	SignalBus.card_used_animation_started.emit(self)
+	_reparent_node_for_animation()
 
+	var tween = get_tree().create_tween()
+	tween.set_trans(Tween.TRANS_CUBIC)
+	tween.tween_method(func(vec: Vector2): position = vec,
+		position,
+		grid_pos.to_pixel() + Vector2.UP*40,
+		0.3
+	)
+
+	tween.finished.connect(func(): 
+		SignalBus.card_used.emit(self, grid_pos)
+		queue_free()
+	)
