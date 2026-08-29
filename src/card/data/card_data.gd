581933b17553406861875b51df38319144784b89
Truyka <mmilewski2202@gmail.com>
2026-08-29 16:17:49 +0200

    rarity changes

--- src/card/data/card_data.gd
+++ src/card/data/card_data.gd
@@ -14,9 +14,9 @@ enum Rarity {
 
 static func rarity_percentage(r: Rarity) -> float:
 	match r:
-		Rarity.COMMON: return 0.50
-		Rarity.UNCOMMON: return 0.30
-		Rarity.RARE: return 0.20
+		Rarity.COMMON: return 0.40
+		Rarity.UNCOMMON: return 0.35
+		Rarity.RARE: return 0.25
 	return 0.0
 
 static func _get_rarity_sum() -> Array[float]:
