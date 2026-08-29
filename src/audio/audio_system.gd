1e6412f1c7e5a81c36d3d4914d190793997dc482
KVBA-dev <kubaokon8@gmail.com>
2026-08-29 14:18:53 +0200

    fixed audio system

--- src/audio/audio_system.gd
+++ src/audio/audio_system.gd
@@ -3,7 +3,6 @@ extends Node
 
 @onready var music_player: AudioStreamPlayer = $Music
 @onready var ambience_player: AudioStreamPlayer = $Ambience
-@onready var sfx_player: AudioStreamPlayer = $SFX
 
 static var instance: AudioSystem
 
@@ -19,10 +18,18 @@ enum SFXType {
 @export var sfx_sounds: Dictionary[SFXType, AudioStream]
 @export var music_streams: Array[AudioStream]
 
+var sfx_streams: Array[AudioStreamPlayer]
 var music_index: int = 0
+var sfx_index: int = 0
 
 func _ready() -> void:
 	instance = self
+	sfx_index = 0
+	for i in range(10):
+		var stream := AudioStreamPlayer.new()
+		stream.volume_db = -3.0
+		add_child(stream)
+		sfx_streams.append(stream)
 	music_index = 0
 	music_player.finished.connect(func(): 
 		music_index = (music_index + 1) % len(music_streams)
@@ -40,5 +47,7 @@ func load_and_play_music() -> void:
 	music_player.play()
 
 func play_sfx(type: SFXType) -> void:
+	var sfx_player := sfx_streams[sfx_index]
+	sfx_index = (sfx_index + 1) % len(sfx_streams)
 	sfx_player.stream = sfx_sounds[type]
 	sfx_player.play()
